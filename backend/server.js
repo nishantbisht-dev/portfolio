a438f844e1551e3d82a16d76cecd5a653b5b1f84
Your Name <you@example.com>
2026-05-19 13:26:58 +0530

    code update due to cors issue policy error occur

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,33 @@
+// const express = require("express");
+// const cors = require("cors");
+// const dotenv = require("dotenv");
+
+// const connectDB = require("./config/db");
+// const contactRoutes = require("./routes/contactRoutes");
+
+// dotenv.config();
+
+// const app = express();
+
+// connectDB();
+
+// app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+
+// app.use(express.json());
+
+// app.get("/", (req, res) => {
+//   res.send("API is running");
+// });
+
+// app.use("/api/contact", contactRoutes);
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
@@ -11,7 +41,25 @@ const app = express();
 
 connectDB();
 
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://portfolio-main-vercel.netlify.app",
+  process.env.CLIENT_URL,
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allows requests from Postman, browser direct requests, and allowed frontend URLs
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
